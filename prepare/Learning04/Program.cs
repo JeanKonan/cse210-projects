77e265b9b37ac5dc215252b70280d33a4ee6e39e
JeanKonan <kon22005@byui.edu>
2023-02-21 23:05:38 -0700

    learning4 completed

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment Assign1 = new Assignment("Roberto Rodriguez", "Fractions");
+        Console.WriteLine($"{Assign1.GetSummary()}");
+        
+        MathAssignment Math1 = new MathAssignment(Assign1.GetStudentName(), Assign1.GetTopic(), "7.3", "8-19");
+        Console.WriteLine($"{Math1.GetHomeworkList()}");
+
+        WritingAssignment Writing1 = new WritingAssignment(Assign1.GetStudentName(), Assign1.GetTopic(), "The causes of World War II");
+        Console.WriteLine($"{Writing1.GetWritingInformation()}");
     }
 }
